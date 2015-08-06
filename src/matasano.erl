152bb097fb57e42fa12675067810996d7a7ebd72
Michael Blockley <michael@sss.co.nz>
2015-08-06 16:49:48 +1200

    completed challenge 2

--- src/matasano.erl
+++ src/matasano.erl
@@ -1,18 +1,66 @@
 -module(matasano).
 
-%% matasano: matasano library's entry point.
-
--export([my_func/0]).
+-export([ % set 1
+          set_1/0,
+          challenge_1_1/0,
+          challenge_1_2/0
+        ]).
 
 
 %% API
 
-my_func() ->
-    ok().
+set_1_list() ->
+    [ {1, fun challenge_1_1/0},
+      {2, fun challenge_1_2/0}
+    ].
 
-%% Internals
+set_1() ->
+    RunFun = fun({I, F}) ->
+            io:format("Set 1 - Challenge ~w - output~n", [I]),
+            R = F(),
+            io:format("~n", []),
+            {I, R}
+    end,
+    PrintFun = fun({I, R}) ->
+            io:format("  Challenge ~w: ~s~n", [I, passed(R)])
+    end,
+    L = lists:map(RunFun, set_1_list()),
+    io:format("~nSet 1~n"),
+    lists:foreach(PrintFun, L).
 
-ok() ->
-    ok.
+challenge_1_1() ->
+    Hex = <<"49276d206b696c6c696e6720796f757220627261696e206c696b6520",
+            "6120706f69736f6e6f7573206d757368726f6f6d">>,
+    Base64 = <<"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzI",
+               "G11c2hyb29t">>,
+    Plain = <<"I'm killing your brain like a poisonous mushroom">>,
+    HexObj = matasano_bytes:new_hex(Hex),
+    ABase64 = matasano_bytes:base64_binary(
+        matasano_bytes:hex_to_base64(HexObj)),
+    APlain = matasano_bytes:hex_to_binary(HexObj),
+    io:format("input hex:       ~s~n", [Hex]),
+    io:format("expected base64: ~s~n", [Base64]),
+    io:format("expected plain:  ~s~n", [Plain]),
+    io:format("actual base64:   ~s~n", [ABase64]),
+    io:format("actual plain:    ~s~n", [APlain]),
+    Base64 =:= ABase64 andalso Plain =:= APlain.
 
-%% End of Module.
+challenge_1_2() ->
+    Hex1 = matasano_bytes:new_hex(<<"1c0111001f010100061a024b53535009181c">>),
+    Hex2 = matasano_bytes:new_hex(<<"686974207468652062756c6c277320657965">>),
+    Hex3 = matasano_bytes:new_hex(<<"746865206b696420646f6e277420706c6179">>),
+    Plain = <<"the kid don't play">>,
+    AHex3 = matasano_bytes:hex_hex_fixed_xor(Hex1, Hex2),
+    APlain = matasano_bytes:hex_to_binary(AHex3),
+    io:format("input hex 1:    ~s~n", [matasano_bytes:hex_binary(Hex1)]),
+    io:format("input hex 2:    ~s~n", [matasano_bytes:hex_binary(Hex2)]),
+    io:format("expected hex:   ~s~n", [matasano_bytes:hex_binary(Hex3)]),
+    io:format("expected plain: ~s~n", [Plain]),
+    io:format("actual hex:     ~s~n", [matasano_bytes:hex_binary(AHex3)]),
+    io:format("actual plain:   ~s~n", [APlain]),
+    Hex3 =:= AHex3 andalso Plain =:= APlain.
+
+%% Helper functions
+
+passed(true) -> "Passed";
+passed(false) -> "Failed".
--- src/matasano_bytes.erl
+++ src/matasano_bytes.erl
@@ -5,12 +5,31 @@
                bytestring/0
              ]).
 
--compile([export_all]).
-%-export([ new_hex/1,
-%          new_base64/1,
-%          hex_to_base64/1,
-%          base64_to_hex/1
-%        ]).
+-export([ % New API
+          new_hex/1,
+          new_base64/1,
+
+          % Extact API
+          hex_binary/1,
+          base64_binary/1,
+
+          % Conversion API
+          %% binary
+          binary_to_hex/1,
+          binary_to_base64/1,
+          %% hex
+          hex_to_binary/1,
+          hex_to_base64/1,
+          %% base64
+          base64_to_binary/1,
+          base64_to_hex/1,
+
+          % Combination API
+          %% binary-binary
+          binary_binary_fixed_xor/2,
+          %% hex-hex
+          hex_hex_fixed_xor/2
+        ]).
 
 -opaque hex()        :: {hex, binary()}.
 -opaque base64()     :: {base64, binary()}.
@@ -26,6 +45,13 @@ new_hex(S) when is_binary(S) -> {hex, S}.
 -spec new_base64(binary()) -> base64().
 new_base64(S) when is_binary(S) -> {base64, S}.
 
+%% Extact API
+-spec hex_binary(hex()) -> binary().
+hex_binary({hex, S}) -> S.
+
+-spec base64_binary(base64()) -> binary().
+base64_binary({base64, S}) -> S.
+
 %% Conversion API
 
 % convert a string binary to a base64 type
@@ -35,23 +61,40 @@ binary_to_base64(S) -> {base64, binary_to_base64(S, <<>>)}.
 % convert a string binary to a hex type
 -spec binary_to_hex(binary()) -> binary().
 binary_to_hex(S) ->
-    Fun = fun(A, X) ->
-            B = byte_to_hex(X),
-            <<A/binary, B>>
+    Fun = fun(X, A) ->
+            B = byte_to_hex(<<X>>),
+            <<A/binary, B/binary>>
     end,
     {hex, binary_foldl(Fun, <<>>, S)}.
 
-%% convert base64 type to a binary string
+% convert base64 type to a binary string
 -spec base64_to_binary(base64()) -> binary().
 base64_to_binary({base64, S}) -> base64_to_binary(S, <<>>).
 
 % convert a base64 type to a hex type
 -spec base64_to_hex(base64()) -> hex().
-base64_to_hex({base64, _String}) -> throw(not_implemented).
+base64_to_hex({base64, S}) -> binary_to_hex(base64_to_binary(S, <<>>)).
+
+% conver a hex type to a binary string
+-spec hex_to_binary(hex()) -> binary().
+hex_to_binary({hex, S}) -> hex_to_binary(S, <<>>).
 
 % convert a hex type to a base64 type
 -spec hex_to_base64(hex() | binary()) -> base64().
-hex_to_base64({hex, _String}) -> throw(not_implemented).
+hex_to_base64({hex, S}) -> binary_to_base64(hex_to_binary(S, <<>>)).
+
+%% Combination Methods
+
+% perform a fixed XOR on two binary objects
+-spec binary_binary_fixed_xor(binary(), binary()) -> binary().
+binary_binary_fixed_xor(A, B) -> binary_binary_fixed_xor(A, B, <<>>).
+
+% perform a fixed XOR on two hex objects
+hex_hex_fixed_xor({hex, A}, {hex, B}) ->
+    binary_to_hex(
+        binary_binary_fixed_xor(
+            hex_to_binary(A, <<>>),
+            hex_to_binary(B, <<>>))).
 
 %% Helper Methods
 
@@ -73,7 +116,12 @@ from_base64_char(63) -> 47.
 
 % convert a hex byte to its a printable representation
 to_hex_char(X) when X >= 0 andalso X =< 9 -> X + 48;
-to_hex_char(X) when X >= 10 andalso X =< 15 -> X + 55.
+to_hex_char(X) when X >= 10 andalso X =< 15 -> X + 87.
+
+% conver a printable hex byte to its binary value
+from_hex_char(X) when X >= 48 andalso X =< 57 -> X - 48;
+from_hex_char(X) when X >= 65 andalso X =< 70 -> X - 55;
+from_hex_char(X) when X >= 97 andalso X =< 102 -> X - 87.
 
 % convert a byte to a printable hex representation
 byte_to_hex(<<A:4, B:4>>) ->
@@ -117,7 +165,7 @@ bytes_to_base64(A) ->
     <<S:6, T:2, U:4, V:4, W:2, X:6>> = A,
     binary_map(fun to_base64_char/1, <<S:8, T:4, U:4, V:6, W:2, X:8>>).
 
-% conver a base64 string to its binary representation
+% convert a base64 string to its binary representation
 -spec base64_to_binary(binary(), binary()) -> binary().
 base64_to_binary(<<>>, Acc) -> Acc;
 base64_to_binary(<<H:32, T/binary>>, Acc) ->
@@ -137,3 +185,23 @@ base64_to_bytes(Str) ->
     DS = binary_map(fun from_base64_char/1, Str),
     <<_:2, S:6, _:2, T:2, U:4, _:2, V:4, W:2, _:2, X:6>> = DS,
     <<S:6, T:2, U:4, V:4, W:2, X:6>>.
+
+% convert a hex string to its binary representation
+-spec hex_to_binary(binary(), binary()) -> binary().
+hex_to_binary(<<>>, Acc) -> Acc;
+hex_to_binary(<<H:16, T/binary>>, Acc) ->
+    BH = hex_to_bytes(<<H:16>>),
+    hex_to_binary(T, <<Acc/binary, BH/binary>>).
+
+% convert 2 hex bytes to a single decimal byte
+hex_to_bytes(<<A, B>>) ->
+    <<X, Y>> = binary_map(fun from_hex_char/1, <<A, B>>),
+    <<X:4, Y:4>>.
+
+% perform fixed XOR on two binary strings
+binary_binary_fixed_xor(<<>>, <<>>, Acc) -> Acc;
+binary_binary_fixed_xor(<<>>, <<_/binary>>, _) -> throw(mismatched_length);
+binary_binary_fixed_xor(<<_/binary>>, <<>>, _) -> throw(mismatched_length);
+binary_binary_fixed_xor(<<A,AT/binary>>, <<B,BT/binary>>, Acc) ->
+    AxB = A bxor B,
+    binary_binary_fixed_xor(AT, BT, <<Acc/binary, AxB>>).
